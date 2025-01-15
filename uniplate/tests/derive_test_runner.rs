25e8cdd64db5b41dd9efffb53ba04739cc5ea431
Felix <felix@felixleitner.com>
2025-01-15 18:47:43 -0500

    split build tests into subdirectories

--- uniplate/tests/derive_test_runner.rs
+++ uniplate/tests/derive_test_runner.rs
@@ -1,7 +1,7 @@
 #[test]
 fn derive_tests() {
     let t = trybuild::TestCases::new();
-    t.compile_fail("tests/derive-fail/*.rs");
-    t.pass("tests/derive-pass/*.rs");
-    t.pass("examples/*.rs");
+    t.compile_fail("tests/derive-fail/**/*.rs");
+    t.pass("tests/derive-pass/**/*.rs");
+    t.pass("examples/**/*.rs");
 }
