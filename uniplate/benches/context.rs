8ecd628b0dcd4308decbd7718940aecad01d224d
Niklas Dewally <niklas@dewally.com>
2025-01-16 16:31:11 +0000

    add basic benchmarks for `contexts_bi`
    Add basic benchmarks for `contexts_bi`, adapted from the `contexts`
    benchmarks.

--- uniplate/benches/context.rs
+++ uniplate/benches/context.rs
@@ -1,7 +1,7 @@
 //! Benchmarks for `context`,`context_bi`
 
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use uniplate::{derive::Uniplate, Uniplate};
+use uniplate::{derive::Uniplate, Biplate as _, Uniplate};
 
 #[derive(PartialEq, Eq, Clone, Uniplate)]
 #[uniplate()]
@@ -20,11 +20,31 @@ fn benchmark_context_derived_enum_big(c: &mut Criterion) {
     });
 }
 
+/// Big benchmark for `context_bi`, using a vector of MyEnums as the input
+///
+/// At time of writing, this is similar to Conjure Oxide, where we keep expressions in a top level vector.
+fn benchmark_context_bi_derived_enum_big(c: &mut Criterion) {
+    let trees: Vec<MyEnum> = (1..5).map(|_| generate_child(1, 3, 0, 7)).collect();
+    c.bench_function("context_bi enum big", |b| {
+        b.iter(|| walk_ctx_bi_vec(black_box(&trees)))
+    });
+}
+
 /// Small benchmark for `context` using a derived Uniplate instance on an enum
 fn benchmark_context_derived_enum_small(c: &mut Criterion) {
     let tree = generate_child(1, 2, 0, 2);
     c.bench_function("context enum small", move |b| {
         b.iter(|| walk_ctx(black_box(&tree)))
+    });
+}
+
+/// Small benchmark for `context_bi`, using a vector of MyEnums as the input
+///
+/// At time of writing, this is similar to Conjure Oxide, where we keep expressions in a top level vector.
+fn benchmark_context_bi_derived_enum_small(c: &mut Criterion) {
+    let trees: Vec<MyEnum> = (1..2).map(|_| generate_child(1, 2, 0, 2)).collect();
+    c.bench_function("context_bi enum small", move |b| {
+        b.iter(|| walk_ctx_bi_vec(black_box(&trees)))
     });
 }
 
@@ -103,9 +123,21 @@ fn walk_ctx(e: &MyEnum) -> &MyEnum {
     black_box(e)
 }
 
+/// Walk using context_bi Bplate<MyEnum> for Vec<MyEnum>
+fn walk_ctx_bi_vec(e: &Vec<MyEnum>) -> &Vec<MyEnum> {
+    for (e1, c) in e.contexts_bi() {
+        black_box(e1.clone());
+        black_box(c.clone());
+        c(e1); // use context to benchmark it too
+    }
+    black_box(e)
+}
+
 criterion_group!(
     benches,
     benchmark_context_derived_enum_big,
-    benchmark_context_derived_enum_small
+    benchmark_context_derived_enum_small,
+    benchmark_context_bi_derived_enum_big,
+    benchmark_context_bi_derived_enum_small
 );
 criterion_main!(benches);
