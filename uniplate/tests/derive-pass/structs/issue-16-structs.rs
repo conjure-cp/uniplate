867d1c90482847a2a07004d96e77021fb816d8e5
Felix <felix@felixleitner.com>
2025-01-26 13:14:30 +0000

    add biplate tests

---
+++ uniplate/tests/derive-pass/structs/issue-16-structs.rs
@@ -0,0 +1,32 @@
+use uniplate::{derive::Uniplate, Biplate};
+
+#[derive(Eq, PartialEq, Clone, Debug, Uniplate)]
+#[uniplate()]
+#[biplate(to=Vec<A>)]
+struct A {
+    children: Vec<A>,
+}
+
+pub fn main() {
+    let inner_as = vec![
+        A {
+            children: vec![A { children: vec![] }],
+        },
+        A { children: vec![] },
+    ];
+    let a = A {
+        children: inner_as.clone(),
+    };
+
+    let result = <_ as Biplate<Vec<A>>>::children_bi(&a)
+        .into_iter()
+        .collect::<Vec<_>>();
+
+    assert_eq!(result.len(), 1);
+    assert_eq!(result[0], inner_as);
+    assert_eq!(<Vec<A> as Biplate<Vec<A>>>::children_bi(&inner_as).len(), 1);
+    assert_eq!(
+        <Vec<A> as Biplate<Vec<A>>>::children_bi(&inner_as)[0],
+        inner_as
+    );
+}
